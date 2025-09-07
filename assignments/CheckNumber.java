a5fb8fcc25bc2f6b31f3c851be4929e679c7cb6a
abikannann-qa <abikannann@gmail.com>
2025-09-07 17:09:49 +0530

    Add files via upload
    Day2 Assignments

---
+++ assignments/CheckNumber.java
@@ -0,0 +1,21 @@
+package week1.day2.assignments;
+
+public class CheckNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version = 10;
+		
+		if (version >0 ) {
+			System.out.println("number is positive");
+		}
+			else {
+				System.out.println("number is negative");
+			}
+			
+		}
+		
+
+	}
+
+
---
+++ assignments/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day2.assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float Version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+ System.out.println("The version is :"+ Version);
+ System.out.println("The developer is :"+ developer);
+ System.out.println("The isBeta is :"+ isBeta);
+ System.out.println("The releaseYear is :"+ releaseYear);
+ System.out.println("The shortcutKey is :"+ shortcutKey);
+		
+	}
+
+}
